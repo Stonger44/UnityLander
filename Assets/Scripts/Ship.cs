91a989d5825960065b741b2a00f4fec6bc2768fa
Stonger <49844364+Stonger44@users.noreply.github.com>
2025-08-20 22:48:11 -0700

    Created prototype level, ship, and impulseStrength and torqueStrength properties

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float _impulseStrength = 10f;
+    [SerializeField] private float _torqueStrength = 10f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
